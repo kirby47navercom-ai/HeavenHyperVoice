5315d227f553b0b8e539e86eb822601ccdfba59f
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2026-07-29 14:31:46 +0900

    게임 컨트롤러 토대

---
+++ Client/Source/HeavenHyperVoice/Player/UEPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UEPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEAVENHYPERVOICE_API AUEPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
