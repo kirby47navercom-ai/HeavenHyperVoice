2bf7ff4bb563c2d9451eed596df00ff535438606
AN MIN YONG <iks05280204@gmail.com>
2026-08-07 19:04:23 +0900

    플레이어 기본 이동 및 서버용 패킷 초기버전

--- Client/Source/HeavenHyperVoice/Character/UEPlayerCharacter.cpp
+++ Client/Source/HeavenHyperVoice/Character/UEPlayerCharacter.cpp
@@ -2,15 +2,11 @@
 
 #include "UEPlayerCharacter.h"
 
-#include "../Data/UEDataAsset.h"
-#include "../UEGameplayTags.h"
+#include "../Component/UEPlayerMovementSyncComponent.h"
 
 #include "Camera/CameraComponent.h"
-#include "EnhancedInputComponent.h"
-#include "EnhancedInputSubsystems.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
-#include "InputAction.h"
 
 AUEPlayerCharacter::AUEPlayerCharacter()
 {
@@ -23,8 +19,6 @@ AUEPlayerCharacter::AUEPlayerCharacter()
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
 	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
-	GetCharacterMovement()->JumpZVelocity = JumpVelocity;
-	GetCharacterMovement()->AirControl = AirControl;
 
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
@@ -34,254 +28,72 @@ AUEPlayerCharacter::AUEPlayerCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+
+	MovementSyncComponent = CreateDefaultSubobject<UUEPlayerMovementSyncComponent>(TEXT("MovementSyncComponent"));
 }
 
 void AUEPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	RefreshMovementSpeed();
-	AddDefaultMappingContext();
 }
 
 void AUEPlayerCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
-	ApplyMovementInput();
-}
-
-void AUEPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	BindInputActions(PlayerInputComponent);
+	ApplyLocalMovementInput();
 }
 
 FVector AUEPlayerCharacter::GetDesiredMovementDirection() const
 {
-	const FVector Direction(MovementInput.X, MovementInput.Y, 0.0f);
+	return GetMoveDirectionFromInput(MovementInput, GetControlRotation());
+}
+
+void AUEPlayerCharacter::SetMovementInput(const FVector2D& NewMovementInput)
+{
+	MovementInput = NewMovementInput.GetClampedToMaxSize(1.0f);
+}
+
+void AUEPlayerCharacter::ApplyLocalMovementInput()
+{
+	const FVector DesiredDirection = GetMoveDirectionFromInput(MovementInput, GetControlRotation());
+	if (DesiredDirection.IsNearlyZero())
+	{
+		return;
+	}
+
+	// Movement follows camera yaw; idle camera rotation does not rotate the character.
+	const float InputStrength = FMath::Clamp(MovementInput.Size(), 0.0f, 1.0f);
+	AddMovementInput(DesiredDirection, InputStrength);
+}
+
+FVector AUEPlayerCharacter::GetCameraForwardAxis(const FRotator& ViewRotation) const
+{
+	const FRotator CameraYawRotation(0.0f, ViewRotation.Yaw, 0.0f);
+	return FRotationMatrix(CameraYawRotation).GetUnitAxis(EAxis::X).GetSafeNormal2D();
+}
+
+FVector AUEPlayerCharacter::GetCameraRightAxis(const FRotator& ViewRotation) const
+{
+	const FRotator CameraYawRotation(0.0f, ViewRotation.Yaw, 0.0f);
+	return FRotationMatrix(CameraYawRotation).GetUnitAxis(EAxis::Y).GetSafeNormal2D();
+}
+
+FVector AUEPlayerCharacter::GetMoveDirectionFromInput(const FVector2D& Input, const FRotator& ViewRotation) const
+{
+	const FVector Direction = GetCameraForwardAxis(ViewRotation) * Input.X + GetCameraRightAxis(ViewRotation) * Input.Y;
 	return Direction.GetSafeNormal();
 }
 
-void AUEPlayerCharacter::BindInputActions(UInputComponent* PlayerInputComponent)
+void AUEPlayerCharacter::ApplyServerMovementCorrection(const FVector& ServerPosition, const FVector& ServerVelocity, const FRotator& ServerRotation, bool bUseHardCorrection)
 {
-	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
-	if (!EnhancedInputComponent || !InputData)
+	const ETeleportType CorrectionTeleportType = bUseHardCorrection ? ETeleportType::TeleportPhysics : ETeleportType::None;
+
+	// Server correction updates the physical character state, then local movement continues next tick.
+	SetActorLocation(ServerPosition, false, nullptr, CorrectionTeleportType);
+	SetActorRotation(ServerRotation, CorrectionTeleportType);
+	if (UCharacterMovementComponent* MovementComponent = GetCharacterMovement())
 	{
-		return;
+		MovementComponent->Velocity = ServerVelocity;
 	}
-
-	const UInputAction* MoveForwardAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_MoveForward);
-	const UInputAction* MoveBackwardAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_MoveBackward);
-	const UInputAction* MoveRightAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_MoveRight);
-	const UInputAction* MoveLeftAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_MoveLeft);
-	const UInputAction* LookYawAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_LookYaw);
-	const UInputAction* LookPitchAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_LookPitch);
-	const UInputAction* RunAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_Run);
-	const UInputAction* RollAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_Roll);
-	const UInputAction* JumpAction = InputData->FindInputActionByTag(UEGameplayTags::Input_Action_Jump);
-
-	if (MoveForwardAction)
-	{
-		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveForward);
-		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Completed, this, &ThisClass::HandleMoveForwardCompleted);
-	}
-
-	if (MoveBackwardAction)
-	{
-		EnhancedInputComponent->BindAction(MoveBackwardAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveBackward);
-		EnhancedInputComponent->BindAction(MoveBackwardAction, ETriggerEvent::Completed, this, &ThisClass::HandleMoveBackwardCompleted);
-	}
-
-	if (MoveRightAction)
-	{
-		EnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveRight);
-		EnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Completed, this, &ThisClass::HandleMoveRightCompleted);
-	}
-
-	if (MoveLeftAction)
-	{
-		EnhancedInputComponent->BindAction(MoveLeftAction, ETriggerEvent::Triggered, this, &ThisClass::HandleMoveLeft);
-		EnhancedInputComponent->BindAction(MoveLeftAction, ETriggerEvent::Completed, this, &ThisClass::HandleMoveLeftCompleted);
-	}
-
-	if (LookYawAction)
-	{
-		EnhancedInputComponent->BindAction(LookYawAction, ETriggerEvent::Triggered, this, &ThisClass::HandleLookYaw);
-	}
-
-	if (LookPitchAction)
-	{
-		EnhancedInputComponent->BindAction(LookPitchAction, ETriggerEvent::Triggered, this, &ThisClass::HandleLookPitch);
-	}
-
-	if (RunAction)
-	{
-		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Started, this, &ThisClass::HandleRunStarted);
-		EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &ThisClass::HandleRunCompleted);
-	}
-
-	if (RollAction)
-	{
-		EnhancedInputComponent->BindAction(RollAction, ETriggerEvent::Started, this, &ThisClass::HandleRollStarted);
-	}
-
-	if (JumpAction)
-	{
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::HandleJumpStarted);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::HandleJumpCompleted);
-	}
-}
-
-void AUEPlayerCharacter::AddDefaultMappingContext() const
-{
-	if (!InputData || !InputData->InputMappingContext)
-	{
-		return;
-	}
-
-	const APlayerController* PlayerController = Cast<APlayerController>(GetController());
-	const ULocalPlayer* LocalPlayer = PlayerController ? PlayerController->GetLocalPlayer() : nullptr;
-	if (!LocalPlayer)
-	{
-		return;
-	}
-
-	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
-	if (InputSubsystem)
-	{
-		InputSubsystem->AddMappingContext(InputData->InputMappingContext, 0);
-	}
-}
-
-void AUEPlayerCharacter::ApplyMovementInput()
-{
-	if (bIsRolling)
-	{
-		AddMovementInput(RollDirection, 1.0f);
-		return;
-	}
-
-	// Project convention:
-	// W increases world +X, S decreases world X.
-	// D increases world +Y, A decreases world Y.
-	const FVector ForwardAxis = FVector::ForwardVector;
-	const FVector RightAxis = FVector::RightVector;
-
-	AddMovementInput(ForwardAxis, MovementInput.X);
-	AddMovementInput(RightAxis, MovementInput.Y);
-}
-
-void AUEPlayerCharacter::RefreshMovementSpeed()
-{
-	GetCharacterMovement()->MaxWalkSpeed = bIsRolling ? RollSpeed : (bIsRunning ? RunSpeed : WalkSpeed);
-	GetCharacterMovement()->JumpZVelocity = JumpVelocity;
-	GetCharacterMovement()->AirControl = AirControl;
-}
-
-void AUEPlayerCharacter::HandleMoveForward(const FInputActionValue& Value)
-{
-	MovementInput.X = Value.Get<float>();
-}
-
-void AUEPlayerCharacter::HandleMoveForwardCompleted(const FInputActionValue& Value)
-{
-	MovementInput.X = 0.0f;
-}
-
-void AUEPlayerCharacter::HandleMoveBackward(const FInputActionValue& Value)
-{
-	MovementInput.X = -Value.Get<float>();
-}
-
-void AUEPlayerCharacter::HandleMoveBackwardCompleted(const FInputActionValue& Value)
-{
-	if (MovementInput.X < 0.0f)
-	{
-		MovementInput.X = 0.0f;
-	}
-}
-
-void AUEPlayerCharacter::HandleMoveRight(const FInputActionValue& Value)
-{
-	MovementInput.Y = Value.Get<float>();
-}
-
-void AUEPlayerCharacter::HandleMoveRightCompleted(const FInputActionValue& Value)
-{
-	MovementInput.Y = 0.0f;
-}
-
-void AUEPlayerCharacter::HandleMoveLeft(const FInputActionValue& Value)
-{
-	MovementInput.Y = -Value.Get<float>();
-}
-
-void AUEPlayerCharacter::HandleMoveLeftCompleted(const FInputActionValue& Value)
-{
-	if (MovementInput.Y < 0.0f)
-	{
-		MovementInput.Y = 0.0f;
-	}
-}
-
-void AUEPlayerCharacter::HandleLookYaw(const FInputActionValue& Value)
-{
-	AddControllerYawInput(Value.Get<float>() * LookYawRate);
-}
-
-void AUEPlayerCharacter::HandleLookPitch(const FInputActionValue& Value)
-{
-	AddControllerPitchInput(Value.Get<float>() * LookPitchRate);
-}
-
-void AUEPlayerCharacter::HandleRunStarted(const FInputActionValue& Value)
-{
-	bIsRunning = true;
-	RefreshMovementSpeed();
-}
-
-void AUEPlayerCharacter::HandleRunCompleted(const FInputActionValue& Value)
-{
-	bIsRunning = false;
-	RefreshMovementSpeed();
-}
-
-void AUEPlayerCharacter::HandleRollStarted(const FInputActionValue& Value)
-{
-	if (bIsRolling || GetWorld()->GetTimeSeconds() < LastRollEndTime + RollCooldown)
-	{
-		return;
-	}
-
-	RollDirection = GetDesiredMovementDirection();
-	if (RollDirection.IsNearlyZero())
-	{
-		RollDirection = GetActorForwardVector().GetSafeNormal2D();
-	}
-
-	bIsRolling = true;
-	RefreshMovementSpeed();
-	GetWorldTimerManager().SetTimer(RollTimerHandle, this, &ThisClass::FinishRoll, RollDuration, false);
-}
-
-void AUEPlayerCharacter::HandleJumpStarted(const FInputActionValue& Value)
-{
-	if (!bIsRolling)
-	{
-		Jump();
-	}
-}
-
-void AUEPlayerCharacter::HandleJumpCompleted(const FInputActionValue& Value)
-{
-	StopJumping();
-}
-
-void AUEPlayerCharacter::FinishRoll()
-{
-	bIsRolling = false;
-	LastRollEndTime = GetWorld()->GetTimeSeconds();
-	RefreshMovementSpeed();
 }
--- Client/Source/HeavenHyperVoice/Player/UEPlayerController.h
+++ Client/Source/HeavenHyperVoice/Player/UEPlayerController.h
@@ -4,9 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputActionValue.h"
 #include "UEPlayerController.generated.h"
 
+class AUEPlayerCharacter;
 class UUELoginWidget;
+class UUEDataAsset;
 
 /**
  * PlayerController for the client entry flow.
@@ -30,12 +33,22 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 	// Blueprint can continue the client flow after C++ has accepted the credentials.
 	UFUNCTION(BlueprintImplementableEvent, Category = "Login", meta = (DisplayName = "On Local Login Succeeded"))
 	void BP_OnLocalLoginSucceeded(const FString& UserId, const FString& Nickname);
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UUEDataAsset> InputData = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input|Look", meta = (ClampMin = "0.0"))
+	float LookYawRate = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input|Look", meta = (ClampMin = "0.0"))
+	float LookPitchRate = 1.0f;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Login")
 	TSubclassOf<UUELoginWidget> LoginWidgetClass;
 
@@ -49,6 +62,26 @@ private:
 	UFUNCTION()
 	void HandleLoginSucceeded(const FString& UserId, const FString& Nickname);
 
+	void AddDefaultMappingContext() const;
+	void BindGameplayInput();
+	void BindMoveInput(class UEnhancedInputComponent* EnhancedInputComponent);
+	void BindLookInput(class UEnhancedInputComponent* EnhancedInputComponent);
+	AUEPlayerCharacter* GetControlledPlayerCharacter() const;
+	void PushMovementInputToCharacter();
+
+	void HandleMoveForward(const FInputActionValue& Value);
+	void HandleMoveForwardStopped(const FInputActionValue& Value);
+	void HandleMoveBackward(const FInputActionValue& Value);
+	void HandleMoveBackwardStopped(const FInputActionValue& Value);
+	void HandleMoveRight(const FInputActionValue& Value);
+	void HandleMoveRightStopped(const FInputActionValue& Value);
+	void HandleMoveLeft(const FInputActionValue& Value);
+	void HandleMoveLeftStopped(const FInputActionValue& Value);
+	void HandleLookYaw(const FInputActionValue& Value);
+	void HandleLookPitch(const FInputActionValue& Value);
+
 	UPROPERTY(Transient)
 	TObjectPtr<UUELoginWidget> LoginWidgetInstance = nullptr;
+
+	FVector2D PendingMovementInput = FVector2D::ZeroVector;
 };
